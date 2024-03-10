f3fdd8a92e65b41b550e33f55035bbcce77a4ef9
jpfelixx <felix.joao@aluno.ifsp.edu.br>
2024-03-10 13:08:42 -0300

    deu certo

---
+++ lista/src/e1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class e1001 {
+    public static void main(String[] args) throws IOException {
+
+        int x;
+        int a,b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        x= a+b;
+
+        System.out.println("X = "+x);
+
+    }
+}
