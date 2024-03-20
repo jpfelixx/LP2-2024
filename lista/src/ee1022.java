6815ab43aeb1c475d8e5dc42c8de67a3d74ac293
jpfelixx <felix.joao@aluno.ifsp.edu.br>
2024-03-19 21:46:10 -0300

    esqueleto

--- lista/src/ee1022.java
+++ lista/src/ee1022.java
@@ -3,12 +3,54 @@ import java.util.Scanner;
 public class ee1022 {
     public static void main(String[] args) {
         
+     Scanner scan = new Scanner (System.in);
+     int a,b,c,d;
+     char barra,op;
+
+     a = scan.nextInt();
+     barra = scan.next().charAt(0);
+     b = scan.nextInt();
     
 
+     op = scan.next().charAt(0);
 
 
+     c = scan.nextInt();
+     barra = scan.next().charAt(0);
+     d = scan.nextInt();
 
+  
 
+   
+
+   
     }
+    public static int calculogeral (char opc, int a, int b,int c, int d){
+   switch (opc) {
+
+    case '+':
+        
+        break;
+
+    case '-':
+        break;
+
+    case '*':
+        break;
+
+    case '/':
+        break;
+
+
+
+    default:
+        break;
+   }
+
+
+        return a;
+    }
+
+   
     
 }
